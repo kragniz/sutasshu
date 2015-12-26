ae7627f33a1ca65de7067468de7392de084230ba
Louis Taylor <louis@kragniz.eu>
2015-12-26 02:36:49 +0000

    Fix django 1.9 shouting at me

--- sutasshu/settings.py
+++ sutasshu/settings.py
@@ -53,12 +53,10 @@ MIDDLEWARE_CLASSES = [
 
 ROOT_URLCONF = 'sutasshu.urls'
 
-TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
-
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
