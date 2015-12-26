fbc340a7d6927289f23f1e9f2600fa029b0e8d6b
Louis Taylor <louis@kragniz.eu>
2015-12-26 02:17:04 +0000

    Add hello world

--- sutasshu/settings.py
+++ sutasshu/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'sutasshuapp',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -51,6 +52,8 @@ MIDDLEWARE_CLASSES = [
 ]
 
 ROOT_URLCONF = 'sutasshu.urls'
+
+TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
 
 TEMPLATES = [
     {
