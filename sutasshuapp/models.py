b4efc8c9972aa271f06ff09206933ec9e50fcb31
Louis Taylor <louis@kragniz.eu>
2015-12-28 19:07:57 +0000

    Add uploading request handling

--- sutasshuapp/models.py
+++ sutasshuapp/models.py
@@ -2,4 +2,8 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Image(models.Model):
+    title = models.CharField(max_length=200)
+    added_date = models.DateTimeField('date added',
+                                      auto_now_add=True)
+    image = models.ImageField()
--- sutasshuapp/views.py
+++ sutasshuapp/views.py
@@ -1,8 +1,29 @@
 from django.shortcuts import render, HttpResponse
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+
+from .forms import UploadForm
+from .models import Image
 
 def index(request):
     return HttpResponse('Hello World!')
 
 
 def add(request):
-    return HttpResponse('Upload image here')
+    if request.method == 'POST':
+        form = UploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            newimage = Image(image=request.FILES['imagefile'])
+            newimage.save()
+
+            return HttpResponseRedirect(reverse('sutasshuapp.views.index'))
+    else:
+        form = UploadForm()
+
+    return render_to_response(
+        'add.html',
+        {'form': form},
+        context_instance=RequestContext(request)
+    )
